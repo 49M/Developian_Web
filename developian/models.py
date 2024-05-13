75020c63c9246a045abb98efd72622d7c63c900c
49M <74563947+49M@users.noreply.github.com>
2024-05-13 11:43:16 -0400

    Admin site created

--- developian/models.py
+++ developian/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Goal(models.Model):
+    """A goal the user wants to work towards and accomplish."""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        """Returns a string representation of the model."""
+        return self.text
