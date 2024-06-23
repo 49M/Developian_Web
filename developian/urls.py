756cb1dd1cd8e883c21722845625fa28589f0638
49M <74563947+49M@users.noreply.github.com>
2024-06-23 15:04:04 -0400

    users new goal creation

--- developian/urls.py
+++ developian/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
     path('goals/', views.goals, name='goals'),
     # Individual Goal Page
     path('goals/<int:goal_id>/', views.goal, name='goal'),
+    # Page for adding new goals
+    path('new_goal/', views.new_goal, name='new_goal'),
 ]
--- developian/views.py
+++ developian/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Goal
+from .forms import GoalForm
 
 # Create your views here.
 def index(request):
@@ -23,3 +24,20 @@ def goal(request, goal_id):
     reflections = goal.reflection_set.order_by('-date_added')
     context = {'goal': goal, 'reflections': reflections}
     return render(request, 'developian/goal.html', context)
+
+def new_goal(request):
+    """
+    Add a new goal for the user.
+    """
+    if request.method != 'POST':
+        # No data submitted; creat a blank form.
+        form = GoalForm()
+    else:
+        # POST data submitted; process data.
+        form = GoalForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('developian:goals')
+    # Display a blank or invalid form
+    context = {'form': form}
+    return render(request, 'developian/new_goal.html', context)
