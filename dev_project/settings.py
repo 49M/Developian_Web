a6164c2fca8f41688c0bbd5160b9d1e519b889b6
49M <74563947+49M@users.noreply.github.com>
2024-06-26 11:32:14 -0400

    Login Functionality

--- dev_project/settings.py
+++ dev_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # My Apps.
     'developian',
+    'accounts',
 
     # Default django apps.
     'django.contrib.admin',
@@ -125,3 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# My Settings
+LOGIN_REDIRECT_URL = 'developian:index'
