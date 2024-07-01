4eee94520851ab5993eeb5c58d50201d45855bec
49M <74563947+49M@users.noreply.github.com>
2024-07-01 17:38:17 -0400

    Navigation bar layout and favicon added

--- dev_project/settings.py
+++ dev_project/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -34,6 +33,9 @@ INSTALLED_APPS = [
     # My Apps.
     'developian',
     'accounts',
+
+    # Third Party Apps
+    'django_bootstrap5',
 
     # Default django apps.
     'django.contrib.admin',
@@ -121,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'developian' / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
