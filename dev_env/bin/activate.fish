02fdb50bda7edd113fd7b512e93a9b568a207257
49M <michal_buczek@yahoo.com>
2024-11-12 09:31:37 -0500

    changed location

--- dev_env/bin/activate.fish
+++ dev_env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/michal/Desktop/Dev/GitHub/Developian_Web/dev_env"
+set -gx VIRTUAL_ENV "/Users/michal/LocalDev/GitHub/Developian_Web/dev_env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
