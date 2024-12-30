ac3a0cb6bf1d65ef7f4ef6bdc143ae446d7ec3ae
buczekmi <74563947+49M@users.noreply.github.com>
2024-12-30 14:05:20 -0500

    improved schedule UI

--- developian/views.py
+++ developian/views.py
@@ -148,6 +148,6 @@ def schedule(request):
     Scheduling page.
     """
     date = datetime.date.today()
-    times = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"]
+    times = ["12:00 am", "1:00 am", "2:00 am", "3:00 am", "4:00 am", "5:00 am", "6:00 am", "7:00 am", "8:00 am", "9:00 am", "10:00 am", "11:00 am", "12:00 pm", "1:00 pm", "2:00 pm", "3:00 pm", "4:00 pm", "5:00 pm", "6:00 pm", "7:00 pm", "8:00 pm", "9:00 pm", "10:00 pm", "11:00 pm"]
     context = {'date': date, 'times': times}
     return render(request, 'developian/schedule.html', context)
